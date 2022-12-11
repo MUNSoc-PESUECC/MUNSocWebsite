cc0c88b6994d5cc44ccf1101519245f55d7261dc
anuragrao04 <raoanu2004@gmail.com>
2022-12-11 21:54:43 +0530

    posts page kinda works

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,5 +1,8 @@
 <script>
-    
+    /**
+	 * @type {{ posts: any; }}
+	 */
+     export let data
 </script>
 
 
@@ -9,10 +12,21 @@
 
 <main>
     <title>MUNSOC-PESUECC | POSTS</title>
-    <body class = "bg-body-slate">
-        <div class = "grid h-screen place-items-center">
-            <img width = 300 src="https://media.tenor.com/Sy2p9OaKxZgAAAAC/nikal-laude-hindistani-bhau.gif" alt="Nikal">
-        </div>
+    <body class = "bg-body-slate text-white">
+        <h1>Posts</h1>
+
+        <ul>
+            {#each data.posts as post}
+              <li>
+                <h2>
+                  <a href={post.path}>
+                    {post.meta.title}
+                  </a>
+                </h2>
+                Published {post.meta.date}
+              </li>
+            {/each}
+          </ul>
     </body>
 </main>
 
