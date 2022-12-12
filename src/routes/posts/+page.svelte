1a9dab5e0195cb47fa83a848c090b2551013448e
anuragrao04 <raoanu2004@gmail.com>
2022-12-12 09:57:53 +0530

    styling posts page

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -13,20 +13,20 @@
 <main>
     <title>MUNSOC-PESUECC | POSTS</title>
     <body class = "bg-body-slate text-white prose prose-invert">
-        <h1 class="p-10">Posts</h1>
-
-        <ul>
-            {#each data.posts as post}
-              <li>
-                <h2>
-                  <a href={post.path}>
-                    {post.meta.title}
-                  </a>
-                </h2>
-                Published {post.meta.date}
-              </li>
-            {/each}
-          </ul>
+        <h1 class="px-10 pt-10 text-center">Posts</h1>
+        <hr>
+        
+        {#each data.posts as post}
+          
+            <h2 class = "px-10">
+              <a href={post.path}>
+                {post.meta.title}
+              </a>
+            </h2>
+            <div class = "px-10">Published {post.meta.date} </div> 
+            <hr>
+        {/each}
+          
     </body>
 </main>
 
