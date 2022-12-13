83ca5437f553e47e307c5a475096a9ae156cb09c
anuragrao04 <raoanu2004@gmail.com>
2022-12-13 12:22:41 +0530

    revert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,10 @@
 <main>
   <title>MUNSOC-PESUECC</title>
   <body class = "bg-body-slate"> 
+    <div class = "wrap">
+      <img class = "wrap-bg" src="/images/logo.png" alt="">
       <div class = "text-white font-mono text-xl px-5 py-5 relative">This Website Is Under Development! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quae veniam ad officia sed delectus distinctio praesentium assumenda commodi, expedita cupiditate voluptate cumque aut. Assumenda culpa consequuntur officiis vitae voluptatibus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo vitae, magnam quam, dolore autem, consectetur cum inventore doloribus corporis itaque quas fugit tempore adipisci. Laborum optio sed, repellat, ab animi recusandae doloremque ullam nisi vero quibusdam facilis nesciunt nostrum. Sunt quo ullam, voluptatibus possimus rerum, omnis magni aperiam nulla, ipsum enim recusandae saepe debitis praesentium neque qui perspiciatis blanditiis modi laboriosam at. Nisi distinctio perspiciatis eaque fugit ipsa sit debitis illo, aspernatur porro culpa veniam dolore est suscipit explicabo nostrum obcaecati, eos accusamus neque doloribus corrupti delectus quis? Molestias, qui totam fugiat esse consequatur culpa maiores assumenda voluptatem dolorem, reiciendis asperiores architecto laborum eveniet consectetur similique nemo cupiditate. Provident magnam maxime repudiandae impedit fugiat, nam modi, excepturi quas aspernatur rerum necessitatibus? Id dicta laboriosam voluptatibus nisi distinctio magni corrupti dolorem voluptates qui eligendi, mollitia quibusdam, laborum sunt voluptatem consequuntur dolor fugit excepturi iste nobis vel dolore rerum repellendus nostrum. Voluptate maxime cumque reiciendis rerum nobis beatae dignissimos hic, quia similique, eos illum ex expedita provident, cum facilis repudiandae. Recusandae molestias velit amet minus facere illum doloribus et? Tempora mollitia maiores consequuntur delectus aut, nesciunt perferendis, omnis quo, quidem vitae laboriosam magni. Illo dignissimos possimus aliquid in pariatur obcaecati perspiciatis praesentium quaerat repellendus corrupti alias, magnam maxime delectus, voluptatum veniam magni quas. Cumque labore dignissimos provident doloremque optio commodi deserunt voluptatibus veniam odit sint, enim pariatur amet aliquid. Alias eaque, commodi beatae ea, maxime saepe necessitatibus nesciunt vitae explicabo ratione repellendus qui, reprehenderit recusandae quam culpa exercitationem aperiam at distinctio perferendis amet! Sed ratione qui rem reiciendis! Laborum quam quod ad minus deserunt ipsa labore nulla porro ipsum reiciendis magnam, sunt eveniet error quas enim nostrum? Quia aspernatur quae corrupti beatae aliquid similique nostrum magni doloribus libero placeat praesentium commodi quod quidem nobis non cupiditate totam eaque est maxime culpa sunt, nemo molestiae? Cumque quasi quas ea commodi ducimus. Aliquam voluptas id quam, consequatur temporibus possimus enim aspernatur architecto amet ratione modi deleniti molestias libero, adipisci fugiat laboriosam! Modi blanditiis ad facere accusantium libero, iusto qui optio excepturi velit sed fuga voluptatem error facilis. Laudantium amet iusto voluptatum excepturi impedit beatae nobis consequuntur, cupiditate illo vitae ex vel veniam ab assumenda? Soluta sint ea dolore! Amet et corporis voluptatibus illum maxime dignissimos odit quisquam nesciunt recusandae pariatur deserunt ad neque veniam placeat voluptates, temporibus, mollitia veritatis assumenda deleniti harum labore? Doloremque necessitatibus ratione pariatur libero enim debitis est, ipsam optio facere dolorem. Beatae eum rerum amet repellat tempora placeat numquam est voluptate. Ut quo architecto eligendi omnis ratione. Dolor aliquam exercitationem, vitae vero minus, distinctio, vel itaque iste rem atque id. Necessitatibus facilis fuga debitis obcaecati dicta fugit quae maiores commodi at rerum sapiente, nihil similique dignissimos sit excepturi impedit! Accusantium, dolore. Maiores, omnis? Officiis recusandae voluptatem expedita, et ducimus exercitationem inventore nostrum ea architecto ipsam rem quidem, quae temporibus animi, ut accusamus veniam sed suscipit quo? Dolores nam sint nesciunt. Officiis praesentium illo ex doloremque repellat magni, nam maiores sunt hic veniam voluptatum numquam culpa aperiam dolorem! Eum, fugit impedit!</div>
-    
+    </div>
 </body>
 </main>
 
@@ -20,22 +22,5 @@
 
 
 <style>
-  body{
-    position: relative;
-  }
-  body:before{
-  content: ' ';
-  display: block;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  opacity: 0.6;
-  background-image: url('/images/logo.png');
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-attachment: fixed;
-  background-position:center;
-  }
+
 </style>
