3200824b573930e13db82cb2d514e303467d6de3
anuragrao04 <raoanu2004@gmail.com>
2022-12-13 19:53:40 +0530

    added images for posts pages too

--- src/routes/posts/[slug]/+page.js
+++ src/routes/posts/[slug]/+page.js
@@ -1,12 +1,14 @@
 // @ts-ignore
 export async function load({ params }){
     const post = await import(`../${params.slug}.md`);
-    const { title, date } = post.metadata;
+    const { title, date, description, img } = post.metadata;
     const content = post.default;
     
     return {
       content,
       title,
       date,
+      description,
+      img,
     }
   }
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -11,6 +11,7 @@
   <body class = "bg-body-slate">
     
   <article class = "text-white p-10 prose prose-invert">
+    <div class = "flex justify-center"><img src="{ data.img }" alt="" class="w-2/3 h-auto rounded-md"></div>
     <h1>{ data.title }</h1>
     <p>Published: {data.date}</p>
     <hr>
