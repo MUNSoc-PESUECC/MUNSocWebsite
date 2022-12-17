791d1befa133a3f25748a5a3094130dc011868df
anuragrao04 <raoanu2004@gmail.com>
2022-12-17 12:27:37 +0530

    posts above events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,6 +20,46 @@
 
 <main>
   <title>MUNSOC-PESUECC</title>
+
+    <!-- POSTS -->
+    <h1 class="p-10 text-center">Recent Posts</h1>
+    
+          <div class="card ml-10 mr-10 rounded-md text-center" onclick="window.location='{data.posts[0].path}'">
+            <div class = "flex justify-center"><img src="{data.posts[0].meta.img}" alt="" class="w-2/3 h-auto rounded-md max-w-md"></div>
+            
+            <div class = "card-content">
+            <h1 class = "pr-10 pl-10">
+              <a href={data.posts[0].path}>
+                {data.posts[0].meta.title}
+              </a>
+            </h1>
+            <h3>✍️ {data.posts[0].meta.author}</h3>
+            <div class = "pl-10 pr-10 text-xl">Published: {data.posts[0].meta.date} </div> 
+            <div class = "pl-10 pr-10">{data.posts[0].meta.description} </div> 
+          </div>
+          </div>
+
+
+          <div class="card ml-10 mr-10 rounded-md text-center" onclick="window.location='{data.posts[0].path}'">
+            <div class = "flex justify-center"><img src="{data.posts[1].meta.img}" alt="" class="w-2/3 h-auto rounded-md max-w-md"></div>
+            
+            <div class = "card-content">
+            <h1 class = "pr-10 pl-10">
+              <a href={data.posts[1].path}>
+                {data.posts[1].meta.title}
+              </a>
+            </h1>
+            <h3>✍️ {data.posts[1].meta.author}</h3>
+            <div class = "pl-10 pr-10 text-xl">Published: {data.posts[1].meta.date} </div> 
+            <div class = "pl-10 pr-10">{data.posts[1].meta.description} </div> 
+          </div>
+          </div>
+
+
+    <hr class="pl-10 pr-10">
+
+
+  <!-- EVENTS -->
   <body class = "bg-body-slate prose prose-invert">
     <h1 class="p-10 text-center">Recent Events</h1>
           <div class="card ml-10 mr-10 rounded-md text-center" onclick="window.location='{data.events[0].path}'">
@@ -57,42 +97,7 @@
 
     <hr class="pl-10 pr-10">
 
-
-    <h1 class="p-10 text-center">Recent Posts</h1>
-    
-          <div class="card ml-10 mr-10 rounded-md text-center" onclick="window.location='{data.posts[0].path}'">
-            <div class = "flex justify-center"><img src="{data.posts[0].meta.img}" alt="" class="w-2/3 h-auto rounded-md max-w-md"></div>
-            
-            <div class = "card-content">
-            <h1 class = "pr-10 pl-10">
-              <a href={data.posts[0].path}>
-                {data.posts[0].meta.title}
-              </a>
-            </h1>
-            <h3>✍️ {data.posts[0].meta.author}</h3>
-            <div class = "pl-10 pr-10 text-xl">Published: {data.posts[0].meta.date} </div> 
-            <div class = "pl-10 pr-10">{data.posts[0].meta.description} </div> 
-          </div>
-          </div>
-
-
-          <div class="card ml-10 mr-10 rounded-md text-center" onclick="window.location='{data.posts[0].path}'">
-            <div class = "flex justify-center"><img src="{data.posts[1].meta.img}" alt="" class="w-2/3 h-auto rounded-md max-w-md"></div>
-            
-            <div class = "card-content">
-            <h1 class = "pr-10 pl-10">
-              <a href={data.posts[1].path}>
-                {data.posts[1].meta.title}
-              </a>
-            </h1>
-            <h3>✍️ {data.posts[1].meta.author}</h3>
-            <div class = "pl-10 pr-10 text-xl">Published: {data.posts[1].meta.date} </div> 
-            <div class = "pl-10 pr-10">{data.posts[1].meta.description} </div> 
-          </div>
-          </div>
-
-
-    <hr class="pl-10 pr-10">
+    <!-- PRESIDENTS -->
 
     <h1 class="p-10 text-center">Meet Our Heads</h1>
     {#each presidents as person}
