4e9348653b26c3b9bf27a153d08d481307cca14b
anuragrao04 <raoanu2004@gmail.com>
2023-01-17 19:48:34 +0530

    about page is not a meme anymore

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,10 +9,14 @@
 
 <main>
     <title>MUNSoc PESU-ECC | ABOUT</title>
-    <body class = "bg-body-slate">
-        <div class = "grid h-screen place-items-center">
-            <img width = 300 src="https://media.tenor.com/Sy2p9OaKxZgAAAAC/nikal-laude-hindistani-bhau.gif" alt="Nikal">
-        </div>
+    <body class = "bg-body-slate prose prose-invert">
+        <h1 class="p-10 text-center">About Us</h1>
+        <h4 class = "pr-32 pl-32">
+            The PESU ECC MUNSoc club was established in the year 2019 by Ashwathi Jayakumar and Akshaya Visvanathan who had noticed the lack of any kinds of debating or diplomatic clubs on the campus. And since then we have established ourselves as one of the most happening and reputed club on the campus.  
+        </h4>
+        <h4 class = "pt-10 pb-10 pr-32 pl-32">
+            In 2021, the MUNSoc legacy was passed on to two very highly accomplished delegates namely Anurag G and Anirudh Rowjee who were one of the first few people to be recruited into the club as members and also the ones who started the streak of winning awards. Under their leadership, our club members have won many Best delegates, High commendations, Special Mentions and so on in a number of distinguished intercollegiate MUNs
+        </h4>
     </body>
 </main>
 
@@ -20,5 +24,14 @@
 
 
 <style>
-
+  .prose{
+    max-width: none;
+  }
+  .prose h1{
+    color: var(--munsoc-gold);
+    text-decoration: none;
+  }
+  .prose h4{
+    font-weight: normal;
+  }
 </style>
