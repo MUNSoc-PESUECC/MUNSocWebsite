bd7651c3ca13539c3d2010def5ff8948be253903
anuragrao04 <raoanu2004@gmail.com>
2023-02-01 13:55:01 +0530

    added margins to pdf newsletters

--- src/routes/newsletter/[slug]/+page.svelte
+++ src/routes/newsletter/[slug]/+page.svelte
@@ -10,9 +10,7 @@
 <main>
   <title>{"NEWSLETTER | " + formattedDate.toUpperCase() }</title>
   <body class = "bg-body-slate">
-    <div style="position: relative; width: 100%; height: 0; padding-top: 141.4286%;
-    padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
-    border-radius: 8px; will-change: transform;">
+    <div class = "canvaEmbed mr-1 ml-1 md:ml-3 md:mr-3">
      <iframe title = "newsletter" loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
        src="{data.embedLink}" allowfullscreen="allowfullscreen" allow="fullscreen">
      </iframe>
@@ -37,6 +35,22 @@
     width:100%;
     padding-left:10%;
     padding-right:10%;
+  }
+
+
+  .canvaEmbed{
+    position: relative;
+    width: 100%; 
+    height: 0; 
+    padding-top: 
+    141.4286%;
+    padding-bottom: 0; 
+    box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); 
+    margin-top: 1.6em; 
+    margin-bottom: 0.9em; 
+    overflow: hidden;
+    border-radius: 8px; 
+    will-change: transform;
   }
 
 
