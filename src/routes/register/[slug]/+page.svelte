fe72f220aac1b07056bfdb687f23943266547796
anuragrao04 <raoanu2004@gmail.com>
2024-02-09 08:35:28 +0530

    part7

--- src/routes/register/[slug]/+page.svelte
+++ src/routes/register/[slug]/+page.svelte
@@ -27,7 +27,9 @@
 			<div
 				class="lg:col-span-2 flex flex-col justify-center items-center text-center text-xl text-munsoc-purple px-2 lg:px-10 uppercase"
 			>
-				<div class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl">
+				<div
+					class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl"
+				>
 					AGENDA
 				</div>
 				Reviewing the role of semiconductor-based microchips in global security with special emphasis
@@ -44,7 +46,9 @@
 			<div
 				class="col-span-2 flex flex-col justify-center items-center text-center text-xl text-munsoc-purple lg:px-10 uppercase"
 			>
-				<div class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl">
+				<div
+					class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl"
+				>
 					AGENDA
 				</div>
 				Deliberating Solutions for the South China Sea Conflict
@@ -60,7 +64,9 @@
 			<div
 				class="col-span-2 flex flex-col justify-center items-center text-center text-xl text-munsoc-purple lg:px-10 uppercase"
 			>
-				<div class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl">
+				<div
+					class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl"
+				>
 					AGENDA
 				</div>
 				Preparing an action plan for access of humanitarian aid in conflict zones with specific reference
@@ -77,7 +83,9 @@
 			<div
 				class="col-span-2 flex flex-col justify-center items-center text-center text-xl text-munsoc-purple lg:px-10 uppercase"
 			>
-				<div class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl">
+				<div
+					class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl"
+				>
 					AGENDA
 				</div>
 				Operation Barbarossa
@@ -93,7 +101,9 @@
 			<div
 				class="col-span-2 flex flex-col justify-center items-center text-center text-xl text-munsoc-purple lg:px-10 uppercase"
 			>
-				<div class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl">
+				<div
+					class="font-le-jour-serif text-munsoc-gold font-semibold m-3 mb-5 lg:text-5xl text-3xl"
+				>
 					AGENDA
 				</div>
 				{LOKSABHA_AGENDA}
@@ -109,58 +119,61 @@
 		</div>
 	{:else if data.redirect == 'Delegation'}
 		<div
-			class="col-span-1 flex flex-col justify-center items-center text-center text-xl text-munsoc-purple lg:px-10 "
+			class="col-span-1 flex flex-col justify-center items-center text-center text-xl text-munsoc-purple lg:px-10"
 		>
 			<div class="font-le-jour-serif text-munsoc-purple m-3 lg:text-4xl text-3xl">
 				GUIDELINES FOR DELEGATION REGISTRATION
 			</div>
-			<div class="mb-4 ml-4 mr-4 mt-2 text-munsoc-purple prose">
-				<h4>
-					1. Visit the MUNSoc PESU ECC Website: <a href="https://munsocecc.vercel.app/"
-						>MUNSoc ECC</a
-					>
-				</h4>
-				<h4>2. Navigate to the UniCon home page and click on the "Register" button.</h4>
-				<h4>3. Select "Register as Delegation" to begin the registration process.</h4>
-				<h4>4. On the bottom of this page, locate the "Register Now as Delegation" button.</h4>
-				<h4>
-					5. A Razorpay page will open. Examine the allocation matrix in the description and consult
-					with your delegation to finalize preferred allocation requests.
-				</h4>
-				<h4>
-					6. Access the Google Sheet in the Payment Details tab questions provided on the Razorpay
-					page.
-				</h4>
-				<h4>
-					7. Duplicate the given sheet and complete the information for your delegation on each
-					committee's page.
-				</h4>
-				<h4>
-					8. Once you've filled in the duplicated sheet, make it visible to everyone for viewing and
-					copy the link.
-				</h4>
-				<div class="font-bold">
-					9. UniCon 2024 mandates each delegation to have at least one delegate in each committee,
-					including IPC (International Press Corps). Failure to comply with this rule will result in
-					non-acceptance of the registration, and a refund will not be processed. Please carefully
-					consider this requirement and ensure that your delegation meets the committee
-					representation criteria before proceeding with the registration process.
+			<div class="text-left px-2 lg:px-10">
+				<div class="mb-4 ml-4 mr-4 mt-2 text-munsoc-purple prose">
+					<h4>
+						1. Visit the MUNSoc PESU ECC Website: <a href="https://munsocecc.vercel.app/"
+							>MUNSoc ECC</a
+						>
+					</h4>
+					<h4>2. Navigate to the UniCon home page and click on the "Register" button.</h4>
+					<h4>3. Select "Register as Delegation" to begin the registration process.</h4>
+					<h4>4. On the bottom of this page, locate the "Register Now as Delegation" button.</h4>
+					<h4>
+						5. A Razorpay page will open. Examine the allocation matrix in the description and
+						consult with your delegation to finalize preferred allocation requests.
+					</h4>
+					<h4>
+						6. Access the Google Sheet in the Payment Details tab questions provided on the Razorpay
+						page.
+					</h4>
+					<h4>
+						7. Duplicate the given sheet and complete the information for your delegation on each
+						committee's page.
+					</h4>
+					<h4>
+						8. Once you've filled in the duplicated sheet, make it visible to everyone for viewing
+						and copy the link.
+					</h4>
+					<div class="font-extrabold">
+						9. UniCon 2024 mandates each delegation to have at least one delegate in each committee,
+						including IPC (International Press Corps). Failure to comply with this rule will result
+						in non-acceptance of the registration, and a refund will not be processed. Please
+						carefully consider this requirement and ensure that your delegation meets the committee
+						representation criteria before proceeding with the registration process.
+					</div>
+					<h4>10. Paste the link in the space provided on the Razorpay page.</h4>
+					<h4>
+						11. Sum up the total number of delegates for whom you've filled in the details on the
+						sheet. Enter this count as the number of delegates you are paying for.
+					</h4>
+					<h4>12. Click "Pay" and proceed to the payment.</h4>
+					<h4>
+						13. After the payment is successful, you will receive a confirmation email from
+						Razorpay.
+					</h4>
+					<p>
+						By following these steps, your delegation will successfully register for UniCon 2024. If
+						you have any questions or concerns, feel free to reach out to <a
+							href="mailto:unicon.pesu@gmail.com">unicon.pesu@gmail.com</a
+						> for assistance.
+					</p>
 				</div>
-				<h4>10. Paste the link in the space provided on the Razorpay page.</h4>
-				<h4>
-					11. Sum up the total number of delegates for whom you've filled in the details on the
-					sheet. Enter this count as the number of delegates you are paying for.
-				</h4>
-				<h4>12. Click "Pay" and proceed to the payment.</h4>
-				<h4>
-					13. After the payment is successful, you will receive a confirmation email from Razorpay.
-				</h4>
-				<p>
-					By following these steps, your delegation will successfully register for UniCon 2024. If
-					you have any questions or concerns, feel free to reach out to <a
-						href="mailto:unicon.pesu@gmail.com">unicon.pesu@gmail.com</a
-					> for assistance.
-				</p>
 			</div>
 		</div>
 	{/if}
@@ -196,5 +209,4 @@
 		font-weight: normal;
 		margin: 6px;
 	}
-	
 </style>
